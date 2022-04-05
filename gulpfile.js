2b39fe4e69d2fd7e5d13cbcd92db797ac092612a
Dima Radushev <86darv@gmail.com>
2022-04-05 13:04:11 +0300

    contacts.pug added; okConfig removed; siteConfig.js added; gulpfile type changed to module; fonts task added; icons-font added; fonts added to src; imagemin added; wather task modified; markup added to base and index

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,81 @@
-const { src, dest, series, parallel, watch } = require('gulp');
-const pug = require('gulp-pug');
-const sass = require('gulp-sass')(require('sass'));
-// const sourcemaps = require('gulp-sourcemaps');
-const gulpif = require('gulp-if');
-const del = require('del');
-const path = require('path');
+import gulp from 'gulp';
+const { src, dest, series, parallel, watch, task } = gulp;
+import pug from 'gulp-pug';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
+import imagemin from 'gulp-imagemin';
+import gulpif from 'gulp-if';
+import del from 'del';
+import path from 'path';
+import siteConfig from './siteConfig.js';
+// const { src, dest, series, parallel, watch } = require('gulp');
+// const pug = require('gulp-pug');
+// const sass = require('gulp-sass')(require('sass'));
+// const imagemin = require('gulp-imagemin');
+// const gulpif = require('gulp-if');
+// const del = require('del');
+// const path = require('path');
 
+// const siteConfig = require('./siteConfig');
 const buildRelease = process.title === 'gulp release';
-const okConfig = require('./okConfig');
 
 
 function markup() {
-    return src(okConfig.paths.root.src + okConfig.paths.markup.src)
-        .pipe(pug({ pretty: true }))
-        .pipe(dest(okConfig.paths.root.dest + okConfig.paths.markup.dest));
+	return src(siteConfig.paths.root.src + siteConfig.paths.markup.src)
+		.pipe(pug({ pretty: '	', doctype: 'html', self: true }))
+		.pipe(dest(siteConfig.paths.root.dest + siteConfig.paths.markup.dest));
 }
 
 function styles() {
-    return src(okConfig.paths.root.src + okConfig.paths.styles.src, gulpif(buildRelease, {}, { sourcemaps: true }))
-        .pipe(sass(gulpif(okConfig.css.compressed, { outputStyle: 'compressed' })).on('error', sass.logError))
-        .pipe(dest(okConfig.paths.root.dest + okConfig.paths.styles.dest, gulpif(buildRelease, {}, { sourcemaps: true })));
+	return src(siteConfig.paths.root.src + siteConfig.paths.styles.src, gulpif(buildRelease, {}, { sourcemaps: true }))
+		.pipe(sass(gulpif(siteConfig.css.compressed, { outputStyle: 'compressed' })).on('error', sass.logError))
+		.pipe(dest(siteConfig.paths.root.dest + siteConfig.paths.styles.dest, gulpif(buildRelease, {}, { sourcemaps: true })));
 }
 
 function scripts() {
-    return src(okConfig.paths.root.src + okConfig.paths.scripts.src, gulpif(buildRelease, {}, { sourcemaps: true }))
-        .pipe(dest(okConfig.paths.root.dest + okConfig.paths.scripts.dest, gulpif(buildRelease, {}, { sourcemaps: true })));
+	return src(siteConfig.paths.root.src + siteConfig.paths.scripts.src, gulpif(buildRelease, {}, { sourcemaps: true }))
+		.pipe(dest(siteConfig.paths.root.dest + siteConfig.paths.scripts.dest, gulpif(buildRelease, {}, { sourcemaps: true })));
+}
+
+function images() {
+	return src(siteConfig.paths.root.src + siteConfig.paths.images.src)
+		.pipe(gulpif(buildRelease, imagemin({ optimizationLevel: 6, progressive: true, silent: true })))
+		.pipe(dest(siteConfig.paths.root.dest + siteConfig.paths.images.dest));
+}
+
+function fonts() {
+	return src(siteConfig.paths.root.src + siteConfig.paths.fonts.src)
+	.pipe(dest(siteConfig.paths.root.dest + siteConfig.paths.fonts.dest));
 }
 
 function clean() {
-    return del([okConfig.paths.root.dest + '/**/*']);
-}
-
-function copyStatic() {
-    return src(okConfig.paths.root.src + okConfig.paths.images.src)
-        .pipe(dest(okConfig.paths.root.dest + okConfig.paths.images.dest));
+	return del([siteConfig.paths.root.dest + '/**/*']);
 }
 
 function watcher() {
-    watch(okConfig.paths.root.src + '/markup', markup);
-    // watch(okConfig.paths.root.src + okConfig.paths.markup.src, markup);
-    watch(okConfig.paths.root.src + okConfig.paths.styles.src, styles);
-    // watch(okConfig.paths.styles.src, styles);
-    // watch(okConfig.paths.styles.src, styles);
-    // watch(okConfig.paths.styles.src, styles);
+	watch(siteConfig.paths.root.src + '/markup', markup);
+	watch(siteConfig.paths.root.src + siteConfig.paths.styles.src, styles);
+	watch(siteConfig.paths.root.src + siteConfig.paths.scripts.src, scripts);
+	watch(siteConfig.paths.root.src + siteConfig.paths.images.src, images);
+	watch(siteConfig.paths.root.src + siteConfig.paths.fonts.src, fonts);
 }
 
 function nmclean() {
-    return del(['node_modules']);
+	return del(['node_modules', 'build', 'package-lock.json']);
 }
 
-// async function drname() {
-//     console.log(__dirname);
-// }
-
-exports.markup = markup;
-exports.styles = styles;
-exports.scripts = scripts;
-exports.clean = clean;
-exports.watcher = watcher;
-exports.nmclean = nmclean;
-exports.release = series(clean, parallel(styles, scripts, copyStatic), markup, watcher);
-exports.default = series(clean, parallel(styles, scripts, copyStatic), markup, watcher);
+task('clean', clean);
+task('nmclean', nmclean);
+task('release', series(clean, parallel(styles, scripts, fonts, images), markup));
+task('default', series(clean, parallel(styles, scripts, fonts, images), markup, watcher));
+// exports.markup = markup;
+// exports.styles = styles;
+// exports.scripts = scripts;
+// exports.images = images;
+// exports.fonts = fonts;
+// exports.clean = clean;
+// exports.watcher = watcher;
+// exports.nmclean = nmclean;
+// exports.release = series(clean, parallel(styles, scripts, fonts, images), markup, watcher);
+// exports.default = series(clean, parallel(styles, scripts, fonts, images), markup, watcher);
---
+++ siteConfig.js
@@ -0,0 +1,31 @@
+export default {
+	"paths": {
+		"root": {
+			"src": "src",
+			"dest": "build"
+		},
+		"markup": {
+			"src": "/markup/pages/**/*.pug",
+			"dest": ""
+		},
+		"styles": {
+			"src": "/styles/**/*.scss",
+			"dest": "/css"
+		},
+		"scripts": {
+			"src": "/scripts/**/*.js",
+			"dest": "/js"
+		},
+		"fonts": {
+			"src": "/fonts/**/*",
+			"dest": "/fonts"
+		},
+		"images": {
+			"src": "/images/**/*",
+			"dest": "/images"
+		}
+	},
+	"css": {
+		"compressed": false
+	}
+}
