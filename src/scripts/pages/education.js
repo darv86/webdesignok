7fd3e2b009b506a42a3021fb7eae3c3e41eab640
Dima R <86darv@gmail.com>
2024-04-04 21:32:35 +0300

    pug and scss files for education page added; education/header done

---
+++ src/scripts/pages/education.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import SnapSlider from '@tannerhodges/snap-slider';
+import { autoplaySnapSlider } from '../libs/snap-slider.js';
+
+const slider = document.querySelector('.testimonials-slider');
+// @ts-ignore
+const snapSlider = new SnapSlider(slider, { start: 'start', loop: true });
+
+autoplaySnapSlider(snapSlider);
