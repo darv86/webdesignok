b7dbea1eba73de461277fcc6fa3b34b3ccd14746
Dima R <86darv@gmail.com>
2024-04-06 22:17:24 +0300

    minor changes

--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -3,8 +3,8 @@
 import SnapSlider from '@tannerhodges/snap-slider';
 import { autoplaySnapSlider } from '../libs/snap-slider.js';
 
-// const slider = document.querySelector('.testimonials-slider');
+const slider = document.querySelector('.testimonials-slider');
 // @ts-ignore
-// const snapSlider = new SnapSlider(slider, { start: 'start', loop: true });
+const snapSlider = new SnapSlider(slider, { start: 'start', loop: true });
 
-// autoplaySnapSlider(snapSlider);
+autoplaySnapSlider(snapSlider);
